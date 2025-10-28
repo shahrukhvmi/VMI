6763393f01cc8e706fd63ee22b834e5fd87eb34c
shahrukhvmi <shahrukh.vmi@gmail.com>
2025-10-28 17:08:47 +0500

    update2

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,6 @@
 // CONSTANT FILE
 
-export const app_url = "https://crm.vmi12.com/wp-json/vmi/v1";
+// export const app_url = "https://crm.vmi12.com/wp-json/vmi/v1";
+export const app_url = process.env.NEXT_PUBLIC_APP_URL;
 export const meta_url = "https://vibrantmediainc.com/";
 export default { app_url, meta_url };
