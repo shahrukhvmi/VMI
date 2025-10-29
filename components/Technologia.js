b808340bc4c7fbecef333388edf1f7e6880f93df
usmanwarind46 <usmanwarind.vmi@gmail.com>
2025-10-29 16:19:39 +0500

    changes

--- components/Technologia.js
+++ components/Technologia.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-function Technologia() {
+function Technologia({ techData }) {
+  console.log(techData, "techData");
+
   return (
-    <div className="tech-main-shadow relative overflow-x-hidden mt-30 z-10 relative">
+    <div className="tech-main-shadow relative overflow-x-hidden mt-30 z-10">
       <div className="tech-main-shadow-wrap">
         <img src="/tech-main-shadow.png" />
       </div>
@@ -10,14 +12,14 @@ function Technologia() {
         <div className="max-container-width w-6xl mx-auto">
           <div className="inner-heading text-center w-full">
             <h2 className="olivera-font">
-              <span className="inner-heading-top">Trusted Tools Behind </span>
+              <span className="inner-heading-top">{techData[3]?.value} </span>
               <br />
-              <span className="inner-heading-span">Every Deliverable</span>
+              <span className="inner-heading-span">{techData[4]?.value}</span>
             </h2>
           </div>
 
           <div className="technologia-logos mt-30">
-            <img src="/tech-logos.png" />
+            <img src={techData[2]?.value} />
           </div>
         </div>
       </div>
