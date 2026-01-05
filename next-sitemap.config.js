6c95b20a4228ef07da2b26aa5d1acde5c11d3c23
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-01-05 13:44:51 +0500

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,30 @@
-// next-sitemap.config.js (CommonJS)
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://vibrantmediainc.com/", // <- hardcode your real domain
+  siteUrl: "https://vibrantmediainc.com",
   generateRobotsTxt: true,
-  outDir: "out", // important for next export
+
   changefreq: "weekly",
   priority: 0.7,
+
   exclude: ["/404", "/500", "/api/*", "/admin/*"],
+
+  additionalPaths: async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_APP_URL}/portfolio-slugs`
+      );
+
+      const items = await res.json();
+
+      // 🔹 Generate ONLY slug pages
+      return items.map((item) => ({
+        loc: `/portfolio/${item.combined_slug}/`,
+        lastmod: item.updated_at,
+        priority: 0.8,
+      }));
+    } catch (error) {
+      console.error("Sitemap slug fetch failed:", error);
+      return [];
+    }
+  },
 };
